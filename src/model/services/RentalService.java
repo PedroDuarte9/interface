006bb819d5d99ab090787204140260958a65f1a5
pedroduarte <p.fmarket1996@gmail.com>
2022-10-22 11:20:06 -0300

    Finalizado com Interface

--- src/model/services/RentalService.java
+++ src/model/services/RentalService.java
@@ -9,38 +9,16 @@ public class RentalService {
     private Double pricePerHour;
     private Double pricePerDay;
 
-    private BrazilTaxService brazilTaxService;
+    private TaxService taxService;
 
 
-    public RentalService(Double pricePerHour, Double pricePerDay, BrazilTaxService brazilTaxService) {
+    public RentalService(Double pricePerHour, Double pricePerDay, TaxService taxService) {
         this.pricePerHour = pricePerHour;
         this.pricePerDay = pricePerDay;
-        this.brazilTaxService = brazilTaxService;
+        this.taxService = taxService;
     }
 
-    public Double getPricePerHour() {
-        return pricePerHour;
-    }
 
-    public void setPricePerHour(Double pricePerHour) {
-        this.pricePerHour = pricePerHour;
-    }
-
-    public Double getPricePerDay() {
-        return pricePerDay;
-    }
-
-    public void setPricePerDay(Double pricePerDay) {
-        this.pricePerDay = pricePerDay;
-    }
-
-    public BrazilTaxService getBrazilTaxService() {
-        return brazilTaxService;
-    }
-
-    public void setBrazilTaxService(BrazilTaxService brazilTaxService) {
-        this.brazilTaxService = brazilTaxService;
-    }
 
     public void processInvoice(CarRental carRental){
         //Esse comando Duration vai calcular o tempo entre um momento inicial e um momento final, neste caso em particular em minutos
@@ -54,9 +32,7 @@ public class RentalService {
             basicPayment = pricePerDay * Math.ceil(hours / 24.0); // Aqui queremos pegar a duração em dias, então dividimos as horas na quantidade de um dia
         }
 
-        double tax = brazilTaxService.tax(basicPayment); // chamando o serviço de taxa e passando por parametro o pagamento para ser calculado REVISAR
-
-
+        double tax = taxService.tax(basicPayment); // chamando o serviço de taxa e passando por parametro o pagamento para ser calculado REVISAR
 
         carRental.setInvoice(new Invoice(basicPayment, tax));
     }
